ab24a1c9ce38c1406cf4bcc1422dafc4dcc8e89c
Nandakumar Abbigeri <officialnandu@gmail.com>
2022-12-19 11:45:24 +0530

    Implemented Unit of Work

---
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBookWeb.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private AppDbContext _appDBContext;
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _appDBContext = appDbContext;
+            Category = new CategoryRepository(_appDBContext);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public void Save()
+        {
+            _appDBContext.SaveChanges();
+        }
+    }
+}
